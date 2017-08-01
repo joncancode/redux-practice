88afd8b6db59777580e79fad301e2ce8a50ea97e
joncancode <jc.haines19@gmail.com>
2017-07-31 21:06:16 -0400

    added middleware to store

--- src/store.js
+++ src/store.js
@@ -1,5 +1,7 @@
-import {createStore} from 'redux'
+import {createStore, applyMiddleware} from 'redux'
 
 import {hotAndColdReducer} from './reducers';
+
+const middleware = []
 
 export default createStore(hotAndColdReducer);
